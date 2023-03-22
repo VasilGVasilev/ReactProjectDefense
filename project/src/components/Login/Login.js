51f5aedd6a6cfc8e00cb9e88b1cde6ed331bd674
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-03-22 20:07:14 +0200

    Login logic implemented

--- project/src/components/Login/Login.js
+++ project/src/components/Login/Login.js
@@ -1,13 +1,37 @@
+import { useContext } from "react";
+import { AuthContext } from '../../contexts/AuthContext'
+
+import * as authService from '../../services/authService'
+
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
 
-    
+    const { userLogin } = useContext(AuthContext)
+    const navigate = useNavigate();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        authService.login(email, password)
+            .then(authData => {
+                userLogin(authData)
+                navigate('/')
+            })
+            .catch(()=>{
+                // navigate('/404')
+            })
+    }
     return(
         <section className='authFormContainer'>
             <div className='formWrapper'>
                 <span className='logo'>Login</span>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input type="email" name="email" placeholder='email'/>
                     <input type="password" name="password" placeholder='password'/>
                     <button>Sign in</button>
